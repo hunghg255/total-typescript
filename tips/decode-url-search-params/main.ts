ced24b27d4bbda3adc09f81451f7349599d4b496
hunghg255 <giahunghust@gmail.com>
2023-01-17 15:07:27 +0700

    update decode url search params

--- tips/decode-url-search-params/main.ts
+++ tips/decode-url-search-params/main.ts
@@ -2,13 +2,20 @@ import { String, Union } from 'ts-toolbelt';
 
 const query = `/home?a=foo&b=wow`;
 
+// Old type
+type QueryParams = {
+  a: 'foo';
+  b: 'wow';
+};
+
+// New Type
 type Query = typeof query;
 
 type SecondQueryPart = String.Split<Query, '?'>[1];
 
 type QueryElements = String.Split<SecondQueryPart, '&'>;
 
-type QueryParams = {
+type NewQueryParams = {
   [QueryElement in QueryElements[number]]: {
     [Key in String.Split<QueryElement, '='>[0]]: String.Split<
       QueryElement,
@@ -17,7 +24,7 @@ type QueryParams = {
   };
 }[QueryElements[number]];
 
-const obj: Union.Merge<QueryParams> = {
+const obj: Union.Merge<NewQueryParams> = {
   a: 'foo',
   b: 'wow',
 };
--- tips/decode-url-search-params/readme.md
+++ tips/decode-url-search-params/readme.md
@@ -5,13 +5,20 @@ import { String, Union } from 'ts-toolbelt';
 
 const query = `/home?a=foo&b=wow`;
 
+// Old type
+type QueryParams = {
+  a: 'foo';
+  b: 'wow';
+};
+
+// New Type
 type Query = typeof query;
 
 type SecondQueryPart = String.Split<Query, '?'>[1];
 
 type QueryElements = String.Split<SecondQueryPart, '&'>;
 
-type QueryParams = {
+type NewQueryParams = {
   [QueryElement in QueryElements[number]]: {
     [Key in String.Split<QueryElement, '='>[0]]: String.Split<
       QueryElement,
@@ -20,7 +27,7 @@ type QueryParams = {
   };
 }[QueryElements[number]];
 
-const obj: Union.Merge<QueryParams> = {
+const obj: Union.Merge<NewQueryParams> = {
   a: 'foo',
   b: 'wow',
 };
