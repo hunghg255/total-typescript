6ae52a32b74aa7305ccdfed83715ab95c9f9779c
hunghg255 <giahunghust@gmail.com>
2023-01-23 09:59:17 +0700

    Record

---
+++  beginners-typescript/11-record.ts
@@ -0,0 +1,62 @@
+// Solution 1
+const createCache1 = () => {
+  const cache: Record<string, string> = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+// Solution 2
+const createCache2 = () => {
+  const cache: {
+    [id: string]: string;
+  } = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+// Solution 3
+interface Cache {
+  [id: string]: string;
+}
+
+const createCache3 = () => {
+  const cache: Cache = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
