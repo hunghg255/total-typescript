66f57d324cabba6f3af0d337dac8a85474cf2e7d
hunghg255 <giahunghust@gmail.com>
2023-02-04 23:26:22 +0700

    02-get-function-parameters

---
+++ type-transformations/01-inference-basics/02-get-function-parameters.ts
@@ -0,0 +1,12 @@
+const makeQuery = (
+  url: string,
+  opts?: {
+    method?: string;
+    headers?: {
+      [key: string]: string;
+    };
+    body?: string;
+  }
+) => {};
+
+type MakeQueryParameters = Parameters<typeof makeQuery>;
