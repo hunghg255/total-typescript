baa83f2c30bf9c8957988a99421e51f3ef0ad0fc
hunghg255 <giahunghust@gmail.com>
2023-02-05 10:40:12 +0700

    06-extract-from-discriminated-union

---
+++ type-transformations/02-unions-and-indexing/06-extract-from-discriminated-union.ts
@@ -0,0 +1,15 @@
+export type Event =
+  | {
+      type: 'click';
+      event: MouseEvent;
+    }
+  | {
+      type: 'focus';
+      event: FocusEvent;
+    }
+  | {
+      type: 'keydown';
+      event: KeyboardEvent;
+    };
+
+type ClickEvent = Extract<Event, { type: 'focus' }>;
