fdb0c3a812aaad315c69fe456ff1f9550044d15a
hunghg255 <giahunghust@gmail.com>
2023-02-05 10:40:30 +0700

    07-exclude-from-discriminated-union

---
+++ type-transformations/02-unions-and-indexing/07-exclude-from-discriminated-union.ts
@@ -0,0 +1,15 @@
+export type Event =
+  | {
+      type: 'click';
+      event: MouseEvent;
+    }
+  | {
+      type: 'focus';
+      event: FocusEvent;
+    }
+  | {
+      type: 'keydown';
+      event: KeyboardEvent;
+    };
+
+type NonKeyDownEvents = Exclude<Event, { type: 'keydown' }>;
